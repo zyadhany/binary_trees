6b0e744030bdbbb365408411c915218b39bbbc33
zyadhany <zeyad.hany2003@gmail.com>
2024-01-04 20:27:52 +0200

    built repo with first task

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -12,7 +12,7 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 		return (0);
 
 	if (node->left || node->right)
-		return (1);
+		return (0);
 
 	return (1);
 }
--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -12,7 +12,7 @@ int binary_tree_is_root(const binary_tree_t *node)
 		return (0);
 
 	if (node->parent)
-		return (1);
+		return (0);
 
 	return (1);
 }
