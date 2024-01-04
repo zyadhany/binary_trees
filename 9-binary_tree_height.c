602d12c149f6e4a020476e77682cfe7b19373948
zyadhany <zeyad.hany2003@gmail.com>
2024-01-04 20:39:02 +0200

    built repo with first task

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - delette binary tree node.
+ * @tree: parent node.
+ *
+ * Return: NULL at error or a pointer to the new node.
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t l, r;
+
+	if (!tree)
+		return (0);
+
+	l = binary_tree_depth(tree->left);
+	r = binary_tree_depth(tree->right);
+
+	return (1 + max(l, r));
+}
