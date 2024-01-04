8fc67e2c4d4b1e1d3c0e5480d9577c3d020aa552
zyadhany <zeyad.hany2003@gmail.com>
2024-01-04 21:17:23 +0200

    added task #14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,28 @@
 #include "binary_trees.h"
 
 /**
+ * bt_height - delette binary tree node.
+ * @tree: parent node.
+ *
+ * Return: NULL at error or a pointer to the new node.
+ */
+int bt_height(const binary_tree_t *tree)
+{
+	int l = 0, r = 0;
+
+	if (!tree)
+		return (0);
+
+	if (tree->left)
+		l = 1 + binary_tree_height(tree->left);
+
+	if (tree->right)
+		r = 1 + binary_tree_height(tree->right);
+
+	return ((l > r) ? l : r);
+}
+
+/**
  * binary_tree_balance - delette binary tree node.
  * @tree: parent node.
  *
@@ -8,9 +30,13 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	int l, r;
 
 	if (!tree)
 		return (0);
 
-	return (1);
+	l = bt_height(tree->left);
+	r = bt_height(tree->right);
+
+	return (l - r);
 }
