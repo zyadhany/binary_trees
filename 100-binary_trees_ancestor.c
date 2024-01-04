1d9cb9e6b6ebaef64a07f162990d1c0580dba972
zyadhany <zeyad.hany2003@gmail.com>
2024-01-04 23:38:17 +0200

    added task #14

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -10,9 +10,26 @@
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 						const binary_tree_t *second)
 {
+	binary_tree_t *a, *b;
 
 	if (!first || !second)
 		return (NULL);
+
+	a = (binary_tree_t *)first;
+
+	while (a)
+	{
+		b = (binary_tree_t *)second;
+
+		while (b)
+		{
+			if (a == b)
+				return (a);
+			b = b->parent;
+		}
+
+		a = a->parent;
+	}
 
 	return (NULL);
 }
