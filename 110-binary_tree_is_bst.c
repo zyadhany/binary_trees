0e97c0117ab894a4184a57a01bde04c3897ea862
zyadhany <zeyad.hany2003@gmail.com>
2024-01-05 01:57:30 +0200

    added bst

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,6 +1,23 @@
 #include "binary_trees.h"
 
 /**
+ * is_bst - delette binary tree node.
+ * @tree: parent node.
+ * @l: a
+ * @r: a
+ * Return: NULL at error or a pointer to the new node.
+ */
+int is_bst(const binary_tree_t *tree, int l, int r)
+{
+	if (!tree)
+		return (1);
+	if (tree->n <= l || tree->n >= r)
+		return (0);
+	return (is_bst(tree->left, l, tree->n) && is_bst(tree->right, tree->n, r));
+}
+
+
+/**
  * binary_tree_is_bst - delette binary tree node.
  * @tree: parent node.
  *
@@ -8,6 +25,7 @@
  */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
-
-	return (1);
+	if (!tree)
+		return (0);
+	return (is_bst(tree, INT_MIN, INT_MAX));
 }
