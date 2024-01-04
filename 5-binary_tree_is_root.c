7536bc9e7120b792a94172bb51448a03e914b7c1
zyadhany <zeyad.hany2003@gmail.com>
2024-01-04 20:26:28 +0200

    built repo with first task

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,17 +1,17 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_leaf - delette binary tree node.
+ * binary_tree_is_root - delette binary tree node.
  * @node: parent node.
  *
  * Return: NULL at error or a pointer to the new node.
  */
-int binary_tree_is_leaf(const binary_tree_t *node)
+int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node == NULL)
 		return (0);
 
-	if (node->left || node->right)
+	if (node->parent)
 		return (1);
 
 	return (1);
