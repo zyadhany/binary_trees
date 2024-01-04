88db28817d30201964039931cab3c33b9d772cf5
zyadhany <zeyad.hany2003@gmail.com>
2024-01-04 21:09:19 +0200

    added task #14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,7 +8,7 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int l, r;
+	size_t l, r;
 
 	if (!tree)
 		return (0);
@@ -16,5 +16,5 @@ int binary_tree_balance(const binary_tree_t *tree)
 	l = binary_tree_height(tree->left);
 	r = binary_tree_height(tree->right);
 
-	return (l - r);
+	return (1);
 }
