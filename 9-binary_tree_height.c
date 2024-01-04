2225720870d2f0a7e1cf17ba8415fec228056502
zyadhany <zeyad.hany2003@gmail.com>
2024-01-04 20:51:19 +0200

    built repo with first task

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,13 +8,16 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t l, r;
+	size_t l = 0, r = 0;
 
 	if (!tree)
 		return (0);
 
-	l = binary_tree_height(tree->left);
-	r = binary_tree_height(tree->right);
+	if (tree->left)
+		l = 1 + binary_tree_height(tree->left);
 
-	return (1 + (l > r) ? l : r);
+	if (tree->right)
+		r = 1 + binary_tree_height(tree->right);
+
+	return ((l > r) ? l : r);
 }
