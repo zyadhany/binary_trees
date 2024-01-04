281b87047493a9fbb9afcd18b7c82203edb1c704
zyadhany <zeyad.hany2003@gmail.com>
2024-01-05 00:05:32 +0200

    added task #14

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -15,8 +15,31 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 		return;
 
 	Q = binary_tree_node(NULL, 0);
+	free(Q);
 	if (!Q)
 		return;
 
-	binary_tree_delete(Q);
+}
+
+/**
+ * binary_tree_node - creat binary tree node.
+ * @parent: parent node.
+ * @value: value of node.
+ *
+ * Return: NULL at error or a pointer to the new node.
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *bt;
+
+	bt = malloc(sizeof(binary_tree_t));
+	if (bt == NULL)
+		return (NULL);
+
+	bt->n = value;
+	bt->parent = parent;
+	bt->left = NULL;
+	bt->right = NULL;
+
+	return (bt);
 }
