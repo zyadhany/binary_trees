f6b67b90a35be023269054c6c23940c34bbb70a1
zyadhany <zeyad.hany2003@gmail.com>
2024-01-04 22:09:32 +0200

    added task #14

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -6,7 +6,7 @@
  *
  * Return: NULL at error or a pointer to the new node.
  */
-binary_tree_t *binary_tree_sibling(const binary_tree_t *node)
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (!node || !node->parent)
 		return (NULL);
