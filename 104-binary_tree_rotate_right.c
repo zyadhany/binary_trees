00277962278dfc2bc77293168accde463b5141ff
zyadhany <zeyad.hany2003@gmail.com>
2024-01-05 01:13:46 +0200

    added task #14

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -8,9 +8,29 @@
  */
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
-	if (!tree)
+	binary_tree_t *tmp;
+
+	if (!tree || !tree->left)
 		return (NULL);
 
-	return (NULL);
+	tmp = tree->left;
+
+	tmp->parent = tree->parent;
+	tree->left = tmp->right;
+	tmp->right = tree;
+	tree->parent = tmp;
+
+	if (tree->left)
+		tree->left->parent = tree;
+
+	if (tmp->parent)
+	{
+		if (tmp->parent->right == tree)
+			tmp->parent->right = tmp;
+		else
+			tmp->parent->left = tmp;
+	}
+
+	return (tmp);
 }
 
