0c625b9efa3c0f2436b502ae6df85d21e66ecedc
zyadhany <zeyad.hany2003@gmail.com>
2024-01-04 20:36:06 +0200

    built repo with first task

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - delette binary tree node.
+ * @tree: parent node.
+ * @func: function to applay
+ *
+ * Return: NULL at error or a pointer to the new node.
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
+}
