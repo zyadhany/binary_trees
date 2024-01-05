74bd3cd9a76b2ee379daf70acded10c9c3f7b255
zyadhany <zeyad.hany2003@gmail.com>
2024-01-05 02:22:51 +0200

    added bst

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -2,13 +2,24 @@
 
 /**
  * array_to_bst - delette binary tree node.
- * @arr: parent node.
+ * @array: parent node.
  * @size: val
  *
  * Return: NULL at error or a pointer to the new node.
  */
 bst_t *array_to_bst(int *array, size_t size)
 {
+	size_t i;
+	bst_t *node;
 
-	return (NULL);
+	if (array == NULL)
+		return (NULL);
+
+	node = NULL;
+
+	for (i = 0; i < size; i++)
+		if (bst_insert(&node, array[i]) == NULL)
+			return (NULL);
+
+	return (node);
 }
