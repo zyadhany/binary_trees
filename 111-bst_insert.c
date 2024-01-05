253e848f5c988a0406b829358027040dd69f0186
zyadhany <zeyad.hany2003@gmail.com>
2024-01-05 02:13:39 +0200

    added bst

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -9,6 +9,43 @@
  */
 bst_t *bst_insert(bst_t **tree, int value)
 {
+	bst_t *at, *tmp;
+
+	if (!tree)
+		return (NULL);
+
+	at = *tree;
+
+	if (at == NULL)
+	{
+		tmp = binary_tree_node(NULL, value);
+		if (tmp == NULL)
+			return (NULL);
+		return (*tree = tmp);
+	}
+
+	if (at->n < value)
+	{
+		if (at->right)
+			return (bst_insert(&at->right, value));
+
+		tmp = binary_tree_node(at, value);
+		if (tmp == NULL)
+			return (NULL);
+		return (at->right = tmp);
+	}
+
+
+	if (at->n > value)
+	{
+		if (at->left)
+			return (bst_insert(&at->left, value));
+
+		tmp = binary_tree_node(at, value);
+		if (tmp == NULL)
+			return (NULL);
+		return (at->left = tmp);
+	}
 
 	return (NULL);
 }
