ca4b95f52c304b27e8ceb48f43ed0fecfbc1c3f2
zyadhany <zeyad.hany2003@gmail.com>
2024-01-05 02:49:57 +0200

    added bst

---
+++ 134-heap_to_sorted_array.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * heap_to_sorted_array - delette binary tree node.
+ * @heap: parent node.
+ * @size: a
+ *
+ * Return: NULL at error or a pointer to the new node.
+ */
+int *heap_to_sorted_array(heap_t *heap, size_t *size)
+{
+	if (!heap)
+		return (NULL);
+	if (!size)
+		return (NULL);
+	return (NULL);
+}
